8cb305991ee2ac2167b9c4660f874e07de644e2c
WoongBae <jeonwb622@gmail.com>
2026-05-22 14:07:39 +0900

    refactor: 다크모드 배경 컬러 일원화 (#13)
    - 영화 상세 보기에서 다크모드 일 때 배경색 일원화 진행

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -19,34 +19,38 @@ function MovieDetail() {
   if (!movieDetailInfo) return <p>로딩 중....</p>;
 
   return (
-    <div
-      className={`movie ${isDarkMode ? 'dark' : 'light'}`}
-      style={{
-        backgroundImage: `
+    <div className={`movieDetail-page ${isDarkMode ? 'dark' : 'light'}`}>
+      <div
+        className="movie"
+        style={{
+          backgroundImage: `
           linear-gradient(to right, rgba(15,32,39,0.8), rgba(32,58,67,0.6), rgba(44,83,100,0.8)), 
           url(${BASE_URL}${movieDetailInfo.backdrop_path})
         `,
-        backgroundSize: 'cover', // 이미지가 화면 전체를 덮도록
-        backgroundPosition: 'center center', // 중앙 기준 배치
-        backgroundRepeat: 'no-repeat', // 반복 안함
-      }}
-    >
-      <div className="detail-poster">
-        <img
-          src={`${BASE_URL}${movieDetailInfo.poster_path}`}
-          alt={`${movieDetailInfo.title}`}
-        />
+          backgroundSize: 'cover', // 이미지가 화면 전체를 덮도록
+          backgroundPosition: 'center center', // 중앙 기준 배치
+          backgroundRepeat: 'no-repeat', // 반복 안함
+        }}
+      >
+        <div className="detail-poster">
+          <img
+            src={`${BASE_URL}${movieDetailInfo.poster_path}`}
+            alt={`${movieDetailInfo.title}`}
+          />
+        </div>
+        <div className="detail-title">{movieDetailInfo.title}</div>
+        <div className="detail-vote_average">
+          평점 : {movieDetailInfo.vote_average}
+        </div>
+        <div className="detail-genre">
+          {movieDetailInfo.genres?.map((genre) => (
+            <Genre key={genre.id} value={genre.name} />
+          ))}
+        </div>
+        <div className="detail-overview">
+          줄거리 : {movieDetailInfo.overview}
+        </div>
       </div>
-      <div className="detail-title">{movieDetailInfo.title}</div>
-      <div className="detail-vote_average">
-        평점 : {movieDetailInfo.vote_average}
-      </div>
-      <div className="detail-genre">
-        {movieDetailInfo.genres?.map((genre) => (
-          <Genre key={genre.id} value={genre.name} />
-        ))}
-      </div>
-      <div className="detail-overview">줄거리 : {movieDetailInfo.overview}</div>
     </div>
   );
 }
